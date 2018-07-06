78d66f594bcb570696b4b2f7da568968dab67e81
Nilansh Bansal <bansalnilansh@gmail.com>
2018-07-06 15:30:40 +0530

    completed

--- eplanning_spider/spiders/eplanning.py
+++ eplanning_spider/spiders/eplanning.py
@@ -9,7 +9,7 @@ class EplanningSpider(scrapy.Spider):
 
     def parse(self, response):
         urls = response.css('a::attr(href)').extract()
-        for url in urls:
+        for url in urls[0:10]:
             if '#' == url:
                 pass
             else:
@@ -27,4 +27,39 @@ class EplanningSpider(scrapy.Spider):
                                         callback = self.parse_pages)
 
     def parse_pages(self,response):
-        pass                                   
+        application_urls = response.css('td > a::attr(href)').extract()
+        for url in application_urls:
+            url = response.urljoin(url)
+            yield Request(url,callback=self.parse_items)
+        next_page_url =  response.css('li.PagedList-skipToNext > a::attr(href)').extract_first()  
+        next_page_url = response.urljoin(next_page_url)
+        yield Request(next_page_url,callback=self.parse_pages)
+    def parse_items(self,response):
+        agent_btn =  response.css('input[value="Agents"]::attr(style)').extract_first()
+        if 'display: inline;  visibility: visible;' in agent_btn:
+            name = response.css('th:contains("Name :") + td::text').extract_first()
+            
+            address_rows = int(response.css('tr > th:contains("Address :")::attr(rowspan)').extract_first())
+            address_first_row_text = response.css('th:contains("Address :") + td::text').extract_first()
+            address_rows_data_after_first = response.css('tr:contains("Address :") ~ tr>td::text').extract()[0:address_rows]
+            
+            complete_address = address_first_row_text 
+            for text in address_rows_data_after_first:
+                complete_address += " " + text.strip()
+            
+            phone = response.css('th:contains("Phone :") + td::text').extract_first()
+            fax = response.css('th:contains("Fax :") + td::text').extract_first()
+            e_mail = response.css('th:contains("e-mail :") + td > a::text').extract_first()
+            url = response.url 
+
+            yield {
+                'name':name,
+                'complete_address':complete_address,
+                'phone':phone,
+                'fax':fax,
+                'e-mail':e_mail,
+                'url':url
+            }
+                
+        else:
+            self.logger.info('Agent button not found on page, passing invalid url. ')
